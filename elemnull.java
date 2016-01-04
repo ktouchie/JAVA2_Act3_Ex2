043dc4f00edcf9e01bdc4dee5af7fbf3412bb9f5
Karin Touchie <ktouchie@gmail.com>
2016-01-04 14:55:46 +0100

    Start over... declare array using null values

--- elemnull.java
+++ elemnull.java
@@ -3,33 +3,28 @@ class elemnull {
     
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
+        Integer[][] array = {{1,null,3},{null,5,6},{7,8,null},{10,11,12}};
         
-        System.out.println("Enter the number of ROWS in the array:");
-        int n = sc.nextInt();
+        double n = array.length;
+        double m = array[0].length;
         
-        System.out.println("Enter the number of COLUMNS in the array:");
-        int m = sc.nextInt();
+        double total = n*m;
         
-        String[][] array = new String[n][m];
-        
-        System.out.println("You will need to enter a total of " + (n*m) + " values to complete the array.");
+        int sum = 0;
         
         for (int i=0; i<n; i++){
             for (int j=0; j<m; j++) {
-                Scanner scan = new Scanner(System.in);  // Without this 2nd scanner system, user unable to enter a first value (output was 2x "Enter a value")
-                System.out.println("Enter a value:");
-                array[i][j] = scan.nextLine();
-            }
-        }
-        
-        System.out.println("Your array is complete:");
-        
-        for (int i=0; i<n; i++){
-            for (int j=0; j<m; j++) {
-                System.out.print(array[i][j] + " ");
+                System.out.print(array[i][j] + "   ");
+                if (array[i][j] == null) {
+                    sum++;
+                }
             }
             System.out.println("");
         }
+        
+        double proportion = sum/total;
+        
+        System.out.println("The proportion of null values is " + 100*proportion + "%.");
+
     }
 }
